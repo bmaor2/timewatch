5b54be685dd55ac4101523e2d5c35771202be74a
Bar Maor <bmaor2@gmail.com>
2023-12-10 17:04:27 +0200

    server side

--- timewatch-app/src/pages/Home/Home.tsx
+++ timewatch-app/src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Avatar, Button, CardContent } from "@mui/material";
 
 import "./home.scss";
 
 const Home: React.FC = () => {
+  const pageWidth = useMemo(() => window.innerWidth, [window.innerWidth]);
   return (
     <div className="home_container">
       <Avatar
@@ -14,13 +15,13 @@ const Home: React.FC = () => {
       />
       <CardContent className="home_text_container">
         <h1
-          style={{ fontSize: `${window.innerWidth / 200}rem` }}
+          style={{ fontSize: `${pageWidth / 200}rem` }}
           className="home_title"
         >
           ברוכים הבאים לאפליקציית Timewatch אוטומטי!
         </h1>
         <p
-          style={{ fontSize: `${window.innerWidth / 300}rem` }}
+          style={{ fontSize: `${pageWidth / 300}rem` }}
           className="home_description"
         >
           כאן תוכלו להגדיר איזור עבודה ושעות פעילות, ואנחנו נפעיל לכם את השעון
@@ -28,7 +29,7 @@ const Home: React.FC = () => {
         </p>
       </CardContent>
       <Button
-        sx={{ fontSize: `${window.innerWidth / 400}rem` }}
+        sx={{ fontSize: `${pageWidth / 400}rem` }}
         variant="contained"
         className="home_button"
       >
